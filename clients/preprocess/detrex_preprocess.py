b2399ef84d6479caaee6490f59f939826d687952
Naeem Iqbal <naeemiqbal996@gmail.com>
2023-05-31 14:05:49 +0200

    Working preprocessing steps for detrex and detectron2

--- clients/preprocess/detrex_preprocess.py
+++ clients/preprocess/detrex_preprocess.py
@@ -18,7 +18,7 @@ class Detrexpreprocess(Preprocess):
         # pad = np.zeros((16, 1280, 3), dtype=np.uint8)
         # cv_image = np.concatenate((cv_image, pad), axis=0)
         # orig = cv_image.copy()
-        cv_image = cv2.cvtColor(cv_image, cv2.COLOR_RGB2BGR)
+        # cv_image = cv2.cvtColor(cv_image, cv2.COLOR_RGB2BGR)
         cv_image = np.transpose(cv_image, (2, 1, 0))
         cv_image = np.expand_dims(cv_image, axis=0)
         # cv_image /= 255.0
