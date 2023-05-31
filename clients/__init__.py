816591c3aa4655d49162c6d716157a5ee52f5891
Naeem Iqbal <naeemiqbal996@gmail.com>
2023-05-31 15:07:58 +0200

    3D detector warnings supressed

--- clients/__init__.py
+++ clients/__init__.py
@@ -1,10 +1,11 @@
+import logging
 from .yolov5_client import Yolov5client
 from .base_client import Client
 from .detectron_client import FCOS_client
 from .detrex_client import Detrex_client
 from .postprocess import *
 from .preprocess import *
-try:
-    from .detector_3d_client import Pointpillars_client
-except ImportError:
-    print("[WARNING] PointPillars client was not imported")
+# try:
+#     from .detector_3d_client import Pointpillars_client
+# except Exception as e:
+#     logging.error(e)
--- clients/preprocess/__init__.py
+++ clients/preprocess/__init__.py
@@ -1,7 +1,8 @@
+import logging
 from .yolov5_preprocess import Yolov5preprocess
 from .detectron_preprocess import FCOSpreprocess
 from .detrex_preprocess import Detrexpreprocess
-try:
-    from .preprocess_3d import PointpillarPreprocess
-except ImportError:
-    print("[WARNING] PointPillars client preprocess was not imported")
+# try:
+#     from .preprocess_3d import PointpillarPreprocess
+# except Exception as e:
+#     logging.error(e)
