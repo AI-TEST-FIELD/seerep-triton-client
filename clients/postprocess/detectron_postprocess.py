b2813baac8cebdd591d5e4e5a3976a2d8f5cae63
Naeem Iqbal <naeemiqbal996@gmail.com>
2023-07-06 14:51:56 +0200

    Detectron2 pre and post process bugfix

--- clients/postprocess/detectron_postprocess.py
+++ clients/postprocess/detectron_postprocess.py
@@ -45,5 +45,6 @@ class FCOSpostprocess(Postprocess):
         scores = np.reshape(scores, prediction.outputs[2].shape)
         class_ids = self.deserialize_bytes_int(prediction.raw_output_contents[3])
         class_ids = np.reshape(class_ids, prediction.outputs[3].shape)
-        
-        return [boxes, class_ids, scores]
+        conf_inds = np.where(scores > 0.40)
+        return [boxes[conf_inds], class_ids[conf_inds], scores[conf_inds]]
+        # return [boxes, class_ids, scores]
