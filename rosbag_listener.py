74db4c0c283353b56454b2b50c987e60e93da0f2
Naeem Iqbal <naeemiqbal996@gmail.com>
2023-12-07 12:25:28 +0100

    Listen to rosbag and visualize pointclouds

--- rosbag_listener.py
+++ rosbag_listener.py
@@ -7,6 +7,7 @@ from sensor_msgs.msg import PointCloud2
 from sensor_msgs import point_cloud2
 from geometry_msgs.msg import PoseWithCovariance
 import numpy as np
+from visual_utils import open3d_vis_utils as visualizer
 # from utils import image_util
 
 
@@ -40,7 +41,20 @@ class RosInference3D():
 
     def _pc_callback(self, msg):
         # TODO what is the 4th attribute of the point clouds from KITTI and what is their data range
-        self.pc = np.array(list(point_cloud2.read_points(msg, field_names = ("x"), skip_nans=True)))
+        self.x = np.array(list(point_cloud2.read_points(msg, field_names = ("x"), skip_nans=True)))
+        self.y = np.array(list(point_cloud2.read_points(msg, field_names = ("y"), skip_nans=True)))
+        self.z = np.array(list(point_cloud2.read_points(msg, field_names = ("z"), skip_nans=True)))
+        self.pc = np.zeros((self.x.shape[0], 3), dtype=np.float64)
+        self.pc[:, 0] = self.x[:, 0]
+        self.pc[:, 1] = self.y[:, 0]
+        self.pc[:, 2] = self.z[:, 0]
+        visualizer.draw_scenes(self.pc)
+        self.i = np.array(list(point_cloud2.read_points(msg, field_names = ("intensity"), skip_nans=True)))
+        self.t = np.array(list(point_cloud2.read_points(msg, field_names = ("t"), skip_nans=True)))
+        self.reflec = np.array(list(point_cloud2.read_points(msg, field_names = ("reflectivity"), skip_nans=True)))
+        self.ring = np.array(list(point_cloud2.read_points(msg, field_names = ("ring"), skip_nans=True)))
+        self.ambient = np.array(list(point_cloud2.read_points(msg, field_names = ("ambient"), skip_nans=True)))
+        self.range = np.array(list(point_cloud2.read_points(msg, field_names = ("range"), skip_nans=True)))
         # self.pc = np.array(list(point_cloud2.read_points(msg)))
         self.pc = self.client_preprocess.filter_pc(self.pc)
         # the number of voxels changes every sample
