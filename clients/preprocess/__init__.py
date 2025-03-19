2ec38c742f72c07d87c57e6fd45971d2bad2b4a9
Naeem Iqbal <naeem.iqbal@dfki.de>
2025-03-19 13:18:09 +0100

    Renamed clients to models

--- clients/preprocess/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging
-from .yolov5_preprocess import Yolov5preprocess
-from .detectron_preprocess import FCOSpreprocess
-from .detrex_preprocess import Detrexpreprocess
-# try:
-#     from .pcdet_preprocess import PCDetPreprocess
-# except Exception as e:
-#     logging.error(e)
